1574f0884ed0136dd8c70baf8e6a0ec599f08395
Rose Mwangi <muthonirosee7@gmail.com>
2025-06-10 17:01:59 +0300

    Add ContextAPI

---
+++ alx-project-0x05/interface/index.tsx
@@ -0,0 +1,13 @@
+export interface PageRouteProps {
+  pageRoute: string
+}
+export interface ButtonProps {
+  buttonLabel: string
+  buttonSize?: string
+  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
+  action?: () => void
+}
+export interface LayoutProps {
+  children: ReactNode;
+}
+import { ReactNode } from "react";
